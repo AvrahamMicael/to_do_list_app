528ace92eeafc8047659a7f8131065288d8cd428
AvrahamMicael <avrahammicael@protonmail.com>
2022-03-28 11:40:39 -0300

    retransmission from index.php and signup.php to pending_tasks.php if _session['authenticated'===true]

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+	session_start();
+	if(isset($_SESSION['authenticated']) && $_SESSION['authenticated'] === true) header('location: pending_tasks.php');
+
+?>
 <html>
 	<head>
 		<meta charset="utf-8">
